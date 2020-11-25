1bbfac111e9b5035400fc5d03aaa902e39b4e681
Gabriel Ferreira <43641763+GFerreira03@users.noreply.github.com>
2020-11-25 15:42:50 -0300

    Base

---
+++ app/src/main/java/br/com/gabrielferreira/moreaqui/MainActivity.java
@@ -0,0 +1,29 @@
+package br.com.gabrielferreira.moreaqui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button novo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        novo = (Button) findViewById(R.id.createNew);
+
+        novo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), insertNewProperty.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/br/com/gabrielferreira/moreaqui/insertNewProperty.java
@@ -0,0 +1,14 @@
+package br.com.gabrielferreira.moreaqui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class insertNewProperty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_new_property);
+    }
+}
