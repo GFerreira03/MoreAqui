6a1f7caca32a59d89186a868388fc56121c72d19
Gabriel Ferreira <43641763+GFerreira03@users.noreply.github.com>
2020-11-27 12:32:12 -0300

    Desabilitar as RadioGroups quando o usuário selecionar "Não Sei"

--- app/src/main/java/br/com/gabrielferreira/moreaqui/Insert.java
+++ app/src/main/java/br/com/gabrielferreira/moreaqui/Insert.java
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class Insert extends AppCompatActivity {
-
+    public String type, size, status, phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +23,70 @@ public class Insert extends AppCompatActivity {
         RadioGroup typeRadiogp = findViewById(R.id.typeRadioGp);
         RadioGroup sizeRadioGp = findViewById(R.id.sizeRadioGp);
         Button confirm_button =  findViewById(R.id.confirm_button);
+        CheckBox dunnoSize = findViewById(R.id.dunnoSize);
+        CheckBox dunnoType = findViewById(R.id.dunnoType);
 
+        typeRadiogp.setOnCheckedChangeListener(((radioGroup, i) -> {
+            switch (i){
+                case R.id.houseRadioBtn:
+                    type = "Casa";
+                    break;
+                case R.id.aptoRadioBtn:
+                    type = "Apartamento";
+                    break;
+                case R.id.shopRadioBtn:
+                    type = "Loja";
+                    break;
+            }
+        }));
+
+        dunnoSize.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (dunnoSize.isChecked()){
+                    size = "Não sei";
+                    sizeRadioGp.clearCheck();
+                    for(int i = 0; i < sizeRadioGp.getChildCount(); i++){
+                        ((RadioButton)sizeRadioGp.getChildAt(i)).setEnabled(false);
+                    }
+                } else {
+                    for(int i = 0; i < sizeRadioGp.getChildCount(); i++){
+                        ((RadioButton)sizeRadioGp.getChildAt(i)).setEnabled(true);
+                    }
+                }
+            }
+        });
+
+        sizeRadioGp.setOnCheckedChangeListener(((radioGroup, i) -> {
+            switch (i){
+                case R.id.smallRadioBtn:
+                    type = "Pequena";
+                    break;
+                case R.id.mediumRadioBtn:
+                    type = "Média";
+                    break;
+                case R.id.bigRadioBtn:
+                    type = "Grande";
+                    break;
+            }
+        }));
+
+        dunnoType.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (dunnoType.isChecked()){
+                    type = "Não sei";
+                    typeRadiogp.clearCheck();
+                    for(int i = 0; i < typeRadiogp.getChildCount(); i++){
+                        ((RadioButton)typeRadiogp.getChildAt(i)).setEnabled(false);
+                    }
+                } else {
+                    for(int i = 0; i < typeRadiogp.getChildCount(); i++){
+                        ((RadioButton)typeRadiogp.getChildAt(i)).setEnabled(true);
+                    }
+                }
+            }
+        });
 
         confirm_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +96,8 @@ public class Insert extends AppCompatActivity {
 
                 if (!numero.matches(expressao)) {
                     Toast.makeText(getApplicationContext(), "Numero Invalido", Toast.LENGTH_SHORT).show();
+                } else {
+
                 }
 
             }
